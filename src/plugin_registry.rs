474084aaec99afcdd20e54689b9ecc755d893994
Vail Dorchester <vail.dorchester@colorado.edu>
2024-03-15 13:24:43 -0600

    still working plugin access

--- src/plugin_registry.rs
+++ src/plugin_registry.rs
@@ -5,7 +5,7 @@ lazy_static::lazy_static! {
     pub static ref PLUGIN_REGISTRY: Mutex<Vec<Box<dyn crate::Plugin>>> = Mutex::new(Vec::new());
 }
 
-pub fn registry() -> plugin_registry::PLUGIN_REGISTRY {
+pub fn registry() -> PLUGIN_REGISTRY {
     PLUGIN_REGISTRY
 }
 
