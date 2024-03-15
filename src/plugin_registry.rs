24a3a4b84ee70015f7d545d818af9e4954992593
Vail Dorchester <vail.dorchester@colorado.edu>
2024-03-15 13:21:02 -0600

    added function to get registry

--- src/plugin_registry.rs
+++ src/plugin_registry.rs
@@ -5,6 +5,10 @@ lazy_static::lazy_static! {
     pub static ref PLUGIN_REGISTRY: Mutex<Vec<Box<dyn crate::Plugin>>> = Mutex::new(Vec::new());
 }
 
+pub fn registry() -> Mutex<Vec<Box<dyn crate::Plugin>>>{
+    PLUGIN_REGISTRY
+}
+
 pub fn register_plugin(plugin: Box<dyn Plugin>){
     let mut registry = PLUGIN_REGISTRY.lock().unwrap();
     registry.push(plugin);
